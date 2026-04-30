5f20bac690dcbfdd786bfa559d9a648a720c019a
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-30 16:00:04 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .logger import get_logger
 from .logger import get_logger
 from .file_utils import save_uploaded_file
 from .ui_utils import show_success, show_error, show_info, save_design, get_design, load_design, set_design
+from .ui_utils import get_design_summary
