f9ea9efb97f1dbe94a46359b4f8e0d04da22a7a1
Sateesh Ambesange ( PragyanAI) <75975970+pragyanaischool@users.noreply.github.com>
2026-09-25 10:43:20 +0530

    Add image display to app layout

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-
+st.image("PragyanAI_Transperent.png")
 # --------------------------------------------------
 # SESSION INIT
 # --------------------------------------------------
