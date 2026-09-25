6bacdde6ef0b870bff0dc751e9545bcefb5523f2
Sateesh Ambesange ( PragyanAI) <75975970+pragyanaischool@users.noreply.github.com>
2026-09-25 11:21:46 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,13 @@
 from .logger import get_logger
-from .logger import get_logger
 from .file_utils import save_uploaded_file
-from .ui_utils import show_success, show_error, show_info, save_design, get_design, load_design, set_design
-from .ui_utils import get_design_summary
+from .ui_utils import (
+    show_success, 
+    show_error, 
+    show_info, 
+    save_design, 
+    get_design, 
+    load_design, 
+    set_design,
+    get_design_summary,
+    download_design_button  # <--- Add this here
+)
